bf1bd016b41c746220c7e71358ed64972c07374f
Giuseppe <giuseppedf8711@gmail.com>
2020-10-22 17:51:17 +0200

    da fare tabella

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+
+class User {
+  puplic $name;
+  puplic $lastname;
+  puplic $email;
+  puplic $username;
+  puplic $password;
+  public $telephone
+
+  public function __construct($username, $email, $password)
+  {
+    $this->username = $_username;
+    $this->email = $_email;
+    $this->password = $_password;
+    $this->telephone = $_telephone;
+
+
+  }
+
+
+}
+
+$user1 = new User("giuseppedf", "giuseppedf1987@yahoo.it", "volley11", "3460157541"); 
+
+$users = [$user1];
+
+
+
+
+ ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
